be34bec12e43bc7ebded806a13e0741c221bba5f
MeAwesome <github@meaweso.me>
2026-05-28 22:04:08 -0400

    fix: ensure the plugin is loaded from this repo's node_modules

--- src/cli.mjs
+++ src/cli.mjs
@@ -6,12 +6,17 @@ import { fileURLToPath } from "url";
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function main() {
   const configPath = fileURLToPath(new URL("./prettier.config.mjs", import.meta.url));
+  const organizeClassMembersPluginPath = fileURLToPath(
+    new URL("../node_modules/prettier-plugin-organize-class-members/build/index.js", import.meta.url)
+  );
 
   const prettierDefaultCommand = `pnpm prettier . --write --config ${JSON.stringify(configPath)}`;
-  const prettierPluginOrganizeClassMembersCommand = `pnpm prettier . --write --config ${JSON.stringify(configPath)} --plugin=prettier-plugin-organize-class-members`;
+  const prettierPluginOrganizeClassMembersCommand = `pnpm prettier . --write --config ${JSON.stringify(configPath)} --plugin=${JSON.stringify(organizeClassMembersPluginPath)}`;
 
   try {
+    console.log("Running Prettier with default configuration...");
     execSync(prettierDefaultCommand, { stdio: "inherit" });
+    console.log("\nRunning Prettier to organize class members...");
     execSync(prettierPluginOrganizeClassMembersCommand, { stdio: "inherit" });
   } catch {
     process.exit(1);
